c00eedcc3493ab65edf516f4a8093c0dae229b06
Lennart Weckeck <lennart.weckeck@gmail.com>
2015-07-01 19:12:25 +0200

    Added Skullstripping workflowstep

--- lesionpypeline/workflow/preprocessing.py
+++ lesionpypeline/workflow/preprocessing.py
@@ -12,23 +12,23 @@ class WorkflowStep(pe.Workflow):
     """TODO"""
     def __init__(self, name):
         super(WorkflowStep, self).__init__(name=name)
-        self._inputnode = pe.Node(interface=nutil.IdentityInterface(fields=['sequences']), name=name+'_inputnode')
-        self._outputnode = pe.Node(interface=nutil.IdentityInterface(fields=['sequences']), name=name+'_outputnode')
+        self._in = pe.Node(interface=nutil.IdentityInterface(fields=['sequences']), name=name+'_in')
+        self._out = pe.Node(interface=nutil.IdentityInterface(fields=['sequences']), name=name+'_out')
 
     @property
-    def inputnode(self):
-        return self._inputnode
+    def in(self):
+        return self._in
 
     @property
-    def outputnode(self):
-        return self._outputnode
+    def out(self):
+        return self._out
 
 class ResamplingStep(WorkflowStep):
     """TODO"""
-    def __init__(self, name, base_index=0):
+    def __init__(self, name, resampling_base_index):
         super(ResamplingStep, self).__init__(name=name)
         
-        baseselector = pe.Node(interface=util.Tee(index=base_index), name=name+'_baseselector')
+        baseselector = pe.Node(interface=util.Tee(index=resampling_base_index), name=name+'_baseselector')
 
         resample = pe.Node(interface=medpy.MedpyResampleTask(), name=name+'_resample')
         resample.inputs.spacing = '3,3,3'
@@ -40,46 +40,53 @@ class ResamplingStep(WorkflowStep):
         reinsert = pe.Node(interface=util.Insert(index=base_index), name=name+'_reinsert')
         
         self.connect([
-            (self.inputnode, baseselector, [('sequences', 'inlist')]),
+            (self.in, baseselector, [('sequences', 'inlist')]),
             (baseselector, resample, [('selected', 'in_file')]),
             (baseselector, registration, [('rejected', 'moving_image')]),
             (resample, registration, [('out_file', 'fixed_image')]),
             (resample, reinsert, [('out_file', 'value')]),
             (registration, reinsert, [('warped_file', 'inlist')]),
-            (reinsert, self.outputnode, [('out', 'sequences')]) ])
+            (reinsert, self.out, [('out', 'sequences')]) ])
 
-class ResamplingStep2(WorkflowStep):
+class SkullstrippingStep(WorkflowStep):
     """TODO"""
-    def __init__(self, name, sequences, base_index=0):
-        super(ResamplingStep, self).__init__(name=name)
+    def __init__(self, name, skullstripping_base_index):
+        super(SkullstrippingStep, self).__init__(name=name)
 
-        baseselector = pe.Node(interface=nutil.Select(index=base_index), name=name+'_baseselector')
-        basefilter = pe.Node(interface.nutil.Select(), name=name+'_basefilter')
-
-        resample = pe.Node(interface=medpy.MedpyResampleTask(), name=name+'_resample')
-        resample.inputs.output_file = sequences[base_index]+'_resampled.nii.gz'
-        resample.inputs.spacing = '3,3,3'
+        baseselector = pe.Node(interface=util.Tee(index=skullstripping_base_index), name=name+'_baseselector')
         
-        registration = pe.MapNode(interface=elastix.Registration(), iterfield='moving_image', name=name+'_registration')
-        registration.inputs.parameters = ['/home/lwe/Projects/LesionPypeline/configs/elastix_sequencespace_rigid_cfg.txt']
-        registration.inputs.terminal_output = 'none'
+        skullstrip = pe.Node(interface=fsl.BET(), name=name+'_skullstrip')
+        skullstrip.inputs.terminal_output = 'none'
+        skullstrip.inputs.mask = True
+        skullstrip.inputs.robust = True
+        skullstrip.inputs.output_type = 'NIFTI_GZ'
+        # HACK
+        skullstrip.interface._cmd = 'fsl5.0-bet'
 
-#        self.connect([
-#            (self.inputnode, baseselector, [('sequences', 'inlist')]),
-#            (self.inputnode, basefilter, [('sequences', 'inlist')]),
-#            (baseselector, resample, [('out', 'input_file')]),
-#            (basefilter, registration, [('out', 'moving_image')]),
-#            (resample, registration, [('output_file', 'fixed_image')]),
-#            (resample, self.outputnode, [('output_file',
+        applymask = pe.MapNode(interface=fsl.ApplyMask(), name=name+'_applymask')
+        applymask.inputs.terminal_output = 'none'
+        # HACK
+        applymask.interface._cmd = 'fsl5.0-fslmaths'
 
+        reinsert = pe.Node(interface=util.Insert(index=base_index), name=name+'_reinsert')
+
+        self.connect([
+            (self.in, baseselector, [('sequences', 'inlist')]),
+            (baseselector, skullstrip, [('selected', 'in_file')]),
+            (baseselector, applymask, [('rejected', 'in_file')]),
+            (skullstrip, applymask, [('mask_file', 'mask_file')]),
+            (skullstrip, reinsert, [('out_file', 'value')]),
+            (applymask, reinsert, [('out_file', 'inlist')]),
+            (reinsert, self.out, [('out', 'sequences')]) ])
+        
 class PreprocessingWorkflow(pe.Workflow):
     """TODO"""
     def __init__(self, name, sequences, registrationbase, skullstrippingbase):
         """"""
         super(PreprocessingWorkflow, self).__init__(name=name)
 
-        self.inputnode = pe.Node(interface=nutil.IdentityInterface(fields=sequences), name='inputnode')
-        self.outputnode = pe.Node(interface=nutil.IdentityInterface(fields=sequences), name='outputnode')
+        self.in = pe.Node(interface=nutil.IdentityInterface(fields=sequences), name='in')
+        self.out = pe.Node(interface=nutil.IdentityInterface(fields=sequences), name='out')
         #TODO resolve name conflict parameters - variables
         self.registrationbase = pe.Node(interface=nutil.IdentityInterface(fields=['base']), name='registrationbase')
         self.skullstrippingbase = pe.Node(interface=nutil.IdentityInterface(fields=['base']), name='skullstrippingbase')
@@ -90,7 +97,7 @@ class PreprocessingWorkflow(pe.Workflow):
                 resample = pe.Node(interface=medpy.MedpyResampleTask(), name=sequence+'_resample')
                 resample.inputs.output_file = 'out.nii.gz'
                 resample.inputs.spacing = '3,3,3'
-                self.connect([(self.inputnode, resample, [(sequence, 'input_file')]),
+                self.connect([(self.in, resample, [(sequence, 'input_file')]),
                               (resample, self.registrationbase, [('output_file', 'base')]),
                               ])
                 prevnode, prevport = resample, 'output_file'
@@ -99,7 +106,7 @@ class PreprocessingWorkflow(pe.Workflow):
                 registration.inputs.parameters = ['/home/lwe/Projects/LesionPypeline/configs/elastix_sequencespace_rigid_cfg.txt']
                 registration.inputs.terminal_output = 'none'
                 self.connect([
-                    (self.inputnode, registration, [(sequence, 'moving_image')]),
+                    (self.in, registration, [(sequence, 'moving_image')]),
                     (self.registrationbase, registration, [('base', 'fixed_image')]),
                     ])
                 prevnode, prevport = registration, 'warped_file'
@@ -130,5 +137,5 @@ class PreprocessingWorkflow(pe.Workflow):
                 prevnode, prevport = applymask, 'out_file'
             # Final Step: Map to Output
             self.connect([
-                (prevnode, self.outputnode, [(prevport, sequence)])
+                (prevnode, self.out, [(prevport, sequence)])
                 ])
