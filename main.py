d03bd637a47ae8c6b2cea08b719130e4cd4d5ee7
Lennart Weckeck <lennart.weckeck@gmail.com>
2015-07-07 16:54:40 +0200

    Adapted main.py to recent cahnges

--- main.py
+++ main.py
@@ -15,39 +15,21 @@ sequences = {
     'dw': 'dw_tra_b1000_dmean',
 }
     
-registration_base = 'flair'
-skullstripping_base = 't1'
-
-# infosource node allows for execution of whole pipline on multiple cases
-infosource = pe.Node(interface=util.IdentityInterface(fields=['case']), name='infosource')
-infosource.iterables = ('case', cases)
-    
-# datasource collects sequence files from case folders
-datasource = pe.Node(interface=nio.DataGrabber(infields=['case'], outfields=sequences.keys()), name='datasource')
-datasource.inputs.base_directory = '/home/lwe/Projects/LesionPypeline/00original'
-datasource.inputs.template = '%s/%s.nii.gz'
-datasource.inputs.sort_filelist = True
-
-info = {sequence: [['case', filename]] for (sequence, filename) in sequences.items()}
-datasource.inputs.template_args = info
 
 # datasink stores output files at given location
 datasink = pe.Node(interface=nio.DataSink(), name='datasink')
 datasink.inputs.container = 'out'
 datasink.inputs.base_directory = '/home/lwe/Projects/LesionPypeline'
 
+datagrabber = pp.assemble_datagrabber_subflow(cases, sequences)
 resampling = pp.assemble_resampling_subflow(sequences.keys(), 'flair')
 skullstripping = pp.assemble_skullstripping_subflow(sequences.keys(), 't1')
 
 metaflow = pe.Workflow(name='metaflow')
-
-metaflow.connect([
-    (infosource, datasource, [('case', 'case')]),
-])
+pp.connect_subflows(metaflow, datagrabber, resampling)
+pp.connect_subflows(metaflow, resampling, skullstripping)
 
 for sequence in sequences.keys():
    metaflow.connect([
-       (datasource, resampling, [(sequence, resampling.inputnode.name+'.'+sequence)]),
-       (resampling, skullstripping, [(resampling.outputnode.name+'.'+sequence, skullstripping.inputnode.name+'.'+sequence)]),
        (skullstripping, datasink, [(skullstripping.outputnode.name+'.'+sequence, sequence)])
    ])
