e295d6aee578380a0d44914276e8b4d870fbe7f2
Lennart Weckeck <lennart.weckeck@gmail.com>
2015-07-09 15:04:44 +0200

    Added cmtk module containing a minimal interface for 'cmtk mrbias' program

---
+++ lesionpypeline/interfaces/cmtk.py
@@ -0,0 +1,23 @@
+import nipype.interfaces.base as base
+import lesionpypeline.util as lutil
+
+class MRBiasInputSpec(base.CommandLineInputSpec):
+    in_file = base.File(desc='the input image', exists=True, mandatory=True, argstr='%s', position=-2)
+    out_file = base.File(desc='the output image', argstr='%s', hash_files=False, position=-1, genfile=True)
+    mask_file = base.File(desc='binary foreground mask', exists=True, argstr='--mask %s')
+
+class MRBiasOutputSpec(base.TraitedSpec):
+    out_file = base.File(desc='the output image', exists=True)
+
+class MRBias(base.CommandLine):
+    """Provides a minimal interface for the cmtk mrbias command"""
+    input_spec = MRBiasInputSpec
+    output_spec = MRBiasOutputSpec
+    cmd = 'cmtk mrbias'
+
+    def _gen_filename(self, name):
+        if name == 'out_file':
+            in_file = self.inputs.get()['in_file']
+            return lutil.append_file_postfix(in_file, '_biasfieldcorrected')
+        else:
+            return None
