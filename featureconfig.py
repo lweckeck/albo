c0c2b0f12ebc52d84f17d7905ee7e885b5340649
Lennart Weckeck <lennart.weckeck@gmail.com>
2015-08-09 00:04:10 +0200

    Rewrote extract_features module, added featureconfig module.

---
+++ featureconfig.py
@@ -0,0 +1,44 @@
+####
+# Configuration file: Denotes the features to extract
+# Formatted as a dictionary with sequences as keys and lists of tuples (function, args, voxelspacing) as values, where function is the
+# feature extraction function, args a dictionary containing the function parameters, and voxelspacing a boolean indicating
+# if the respective voxelspacing needs to be passed to the function.
+####
+
+from medpy.features.intensity import intensities, centerdistance, centerdistance_xdminus1, local_mean_gauss, local_histogram
+from medpy.features.intensity import indices as indices_feature
+
+features_to_extract = {
+    'flair_tra': [
+        (intensities, dict(), False),
+	(local_mean_gauss, dict(sigma=3), True),
+	(local_mean_gauss, dict(sigma=5), True),
+	(local_mean_gauss, dict(sigma=7), True),
+	(local_histogram, dict(bins=11, rang='image', cutoffp=(0, 100), size=5,  footprint=None, output=None, mode='ignore', origin=0), False), #11 bins, 5*2=10mm region
+	(local_histogram, dict(bins=11, rang='image', cutoffp=(0, 100), size=10, footprint=None, output=None, mode='ignore', origin=0), False), #11 bins, 10*2=20mm region
+	(local_histogram, dict(bins=11, rang='image', cutoffp=(0, 100), size=15, footprint=None, output=None, mode='ignore', origin=0), False), #11 bins, 15*2=30mm region
+	(centerdistance_xdminus1, dict(dim=0), True),
+	(centerdistance_xdminus1, dict(dim=1), True),
+	(centerdistance_xdminus1, dict(dim=2), True)
+    ], 
+    
+    'dw_tra_b1000_dmean': [
+        (intensities, dict(), False),
+	(local_mean_gauss, dict(sigma=3), True),
+	(local_mean_gauss, dict(sigma=5), True),
+	(local_mean_gauss, dict(sigma=7), True),
+	(local_histogram, dict(bins=11, rang='image', cutoffp=(0, 100), size=5,  footprint=None, output=None, mode='ignore', origin=0), False), #11 bins, 5*2=10mm region
+	(local_histogram, dict(bins=11, rang='image', cutoffp=(0, 100), size=10, footprint=None, output=None, mode='ignore', origin=0), False), #11 bins, 10*2=20mm region
+	(local_histogram, dict(bins=11, rang='image', cutoffp=(0, 100), size=15, footprint=None, output=None, mode='ignore', origin=0), False), #11 bins, 15*2=30mm region
+    ],
+
+    't1_sag_tfe': [
+        (intensities, dict(), False),
+	(local_mean_gauss, dict(sigma=3), True),
+	(local_mean_gauss, dict(sigma=5), True),
+	(local_mean_gauss, dict(sigma=7), True),
+	(local_histogram, dict(bins=11, rang='image', cutoffp=(0, 100), size=5,  footprint=None, output=None, mode='ignore', origin=0), False), #11 bins, 5*2=10mm region
+	(local_histogram, dict(bins=11, rang='image', cutoffp=(0, 100), size=10, footprint=None, output=None, mode='ignore', origin=0), False), #11 bins, 10*2=20mm region
+	(local_histogram, dict(bins=11, rang='image', cutoffp=(0, 100), size=15, footprint=None, output=None, mode='ignore', origin=0), False), #11 bins, 15*2=30mm region
+    ],
+}
--- lesionpypeline/utility/extract_features.py
+++ lesionpypeline/utility/extract_features.py
@@ -1,60 +1,67 @@
 #!/usr/bin/python
 
-"""
-Extract features from an supplied multi-spectral image according to a config file and saves them under the supplied target directory.
-arg1: folder with image channels
-arg2: mask image, features are only extracted for voxels where 1
-arg3: the target folder to store the extracted features
-arg4: the config file, containing a struct called features_to_extract that follows a special syntax
-
-Note: Does not overwrite existing feature files.
-"""
-
 import os
 import sys
 import imp
 import numpy
 import itertools
+import medpy.io as mio
 
-from medpy.io import load, header
+target_dtype = numpy.float32
+"""type: Numpy-datatype to use for saving the extracted feature vector"""
 
-# configuration
-trg_dtype = numpy.float32
+def extract_features(features, image_paths, mask_file, out_dir):
+    """Extract features from the given images, according to the given feature configuration.
 
-def extract_features(in_dir, mask_file, out_dir, config_file):
-	# loading the features to extract
-	d, m = os.path.split(os.path.splitext(config_file)[0])
-	f, filename, desc = imp.find_module(m, [d])
-	features_to_extract = imp.load_module(m, f, filename, desc).features_to_extract
+    Args:
+        features (dict): Dictionary where keys are sequence identifiers and values are tuples
+            (function, kwargs, voxelspacing?). function is a python function handle, kwargs a
+            dictionary of named parameters, and voxelspacing? a boolean stating if the image's
+            voxelspacing must be passed as an additional parameter.        
+        image_paths (dict(str: str)): Dictionary that contains the paths to the images the
+            features are to be extracted from as pairs "sequence_id: path".
+            Sequence identifiers must be consistent with feature_config_file!
+        mask_file (str): path to the mask file which restricts the area where features are extracted
+        out_dir (str): path to save the resulting feature vectors to
+    """
 
-	# loading the image mask
-	m = load(mask_file)[0].astype(numpy.bool)
+    mask = mio.load(mask_file)[0].astype(numpy.bool)
 
-	# extracting the required features and saving them
-	for sequence, function_call, function_arguments, voxelspacing in features_to_extract:
-		if not isfv(out_dir, sequence, function_call, function_arguments):
-			#print sequence, function_call.__name__, function_arguments
-			i, h = load('{}/{}.nii.gz'.format(in_dir, sequence))
-			call_arguments = list(function_arguments)
-			if voxelspacing: call_arguments.append(header.get_pixel_spacing(h))
-			call_arguments.append(m)
-			fv = function_call(i, *call_arguments)
-			savefv(fv, out_dir, sequence, function_call, function_arguments)
+    for sequence, features in features.items():
+        image, header = mio.load(image_paths[sequence])
+        for function_to_apply, kwargs, voxelspacing in features:
+            kwargs['mask'] = mask
+            if voxelspacing:
+                kwargs['voxelspacing'] = mio.header.get_pixel_spacing(header)
+                
+            feature_vector = function_to_apply(image, **kwargs)
+            filename = generate_feature_filename(sequence, function_to_apply, kwargs)
+            save_feature_vector(feature_vector, out_dir, filename)
 
-def savefv(fv, trgdir, seq, fcall, fargs):
-	"""Saves the supplied feature vector under a fixed naming rule."""
-	name = 'feature.{}.{}.{}'.format(seq, fcall.func_name, '_'.join(['arg{}'.format(i) for i in fargs]))
-	with open('{}/{}.npy'.format(trgdir, name), 'wb') as f:
-		numpy.save(f, fv.astype(trg_dtype))
+def load_feature_config(feature_config_file):
+    """Loads the feature configuration dictionary from the given file.
 
-def isfv(trgdir, seq, fcall, fargs):
-	name = 'feature.{}.{}.{}'.format(seq, fcall.func_name, '_'.join(['arg{}'.format(i) for i in fargs]))
-	return os.path.exists('{}/{}.npy'.format(trgdir, name))
+    Args:
+        feature_config_file (str): Path to a feature configuration file. The file must be a python
+        module containing a dict called "features_to_extract".
+    """
+    directory, name = os.path.split(feature_config_file)
+    module, _ = os.path.splitext(name)
+    f, filename, desc = imp.find_module(module, [directory])
+    features = imp.load_module(module, f, filename, desc).features_to_extract
 
-if __name__ == "__main__":
-	in_dir = sys.argv[1]
-        mask_file = sys.argv[2]
-        out_dir = sys.argv[3]
-        config_file = sys.argv[4]
-        
-        extract_features(in_dir, mask_file, out_dir, config_file)
+    return features
+
+def generate_feature_filename(sequence, function, kwargs):
+    """Generates a file name for the given feature vector configuration.
+    """
+    argstrings = ('arg{}'.format(value) for key, value in kwargs.items()
+                  if key not in {'mask', 'voxelspacing'})
+    return 'feature.{}.{}.{}'.format(sequence, function.func_name, '_'.join(argstrings))
+
+def save_feature_vector(feature_vector, target_dir, filename):
+    """Saves the supplied feature vector as target_dir/filename.npy
+    """
+    path = '{}/{}.npy'.format(target_dir, filename)
+    with open(path, 'wb') as f:
+        numpy.save(f, feature_vector.astype(target_dtype))
